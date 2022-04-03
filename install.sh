f1307cbe25450a1ae6d89c96b952f5a6977c9d98
Azrael JD <94840719+azraeljd@users.noreply.github.com>
2022-04-03 20:20:58 +0700

    Add TermDM installer

---
+++ install.sh
@@ -0,0 +1,32 @@
+# TermDM Installer
+#
+# Installer for the TermDM display manager.
+#
+# Copyright (c) 2022 Azrael JD.
+#
+# This software is freely usable and distributable under
+# the terms of the Azratek Free and Open Source Software
+# License.
+#
+# Disclaimer:
+# THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY. IN NO EVENT SHALL
+# THE AUTHORS, CONTRIBUTORS, NOR THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# DAMAGE CAUSED BY THE USE OF THIS SOFTWARE.
+
+#!/bin/bash
+
+echo "Installing TermDM..."
+
+cp dotfiles/.xinitrc $HOME/.xinitrc
+cp .termdm $HOME/.termdm
+
+read -p "Start TermDM at login? " TERMDM_START_ON_LOGIN
+case $TERMDM_START_ON_LOGIN in
+  "y")
+    echo "bash $HOME/.termdm/termdm" >> $HOME/.bashrc
+    ;;
+  "n")
+    ;;
+esac
+
+echo "TermDM has been installed."
